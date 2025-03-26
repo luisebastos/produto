3b984405f61ab6bafd86135cabb854e8bc2a8a2f
luisebastos <luise@gustavobastos.com.br>
2025-03-25 23:30:28 -0300

    oi

---
+++ src/test/java/com/example/demo/produto/test/TesteControllerTests.java
@@ -0,0 +1,35 @@
+package com.example.demo.produto.test;
+
+import com.example.demo.teste.TesteController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@ExtendWith(MockitoExtension.class)
+public class TesteControllerTests {
+
+    @InjectMocks
+    private TesteController testeController;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        this.mockMvc = MockMvcBuilders
+                .standaloneSetup(testeController)
+                .build();
+    }
+
+    @Test
+    void test_TestHelloWorld() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/teste"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+}
